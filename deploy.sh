7730206aab17bc4ef6b11b20d41cd2d1cec03806
Sery, Eran <eran.sery@delta.com>
2021-05-06 02:41:15 +0000

    changes in IAM

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ AWS_ACCOUNT=$(aws sts get-caller-identity --output text --query Account)
 echo "ACCOUNT_ID=$AWS_ACCOUNT"
 
 if ! aws cloudformation describe-stacks --stack-name ${stackname}-${AWS_DEFAULT_REGION} --region ${AWS_DEFAULT_REGION} >/dev/null 2>&1; then
+   echo "stack does not exist, validating first"
+   aws cloudformation validate-template --template-body file://cft/pipeline_lambda.yaml
    echo "stack does not exist, creating"
    aws --region ${AWS_DEFAULT_REGION} cloudformation deploy \
    --stack-name ${stackname}-${AWS_DEFAULT_REGION} \
