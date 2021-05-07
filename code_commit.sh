dccfe04fb768883eb6959d7cb9afbf57bf45931d
Sery, Eran <eran.sery@delta.com>
2021-05-07 12:41:02 +0000

    add code_commit.sh

---
+++ code_commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+
+# Zip the custom source lambda for publishing
+zip -q -r9 learning-circle-app.zip aws-lambda-101/
+
+# Publish the pipeline prereq objects
+aws s3 cp learning-circle-app.zip s3://"$BUCKET_NAME"/
+
+rm -rf *.zip
